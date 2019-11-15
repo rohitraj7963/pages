38983285b1e0f1b1df0083fb5f97ad28182dbba4
Hritikkumar <kumarhritik38@gmail.com>
2019-11-15 22:28:56 +0530

    error solve 1

--- pages_project/urls.py
+++ pages_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from testapp import views
+from pages import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
